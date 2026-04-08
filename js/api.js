c8cdd363b51a50e4057dc90b16b54600abc64b99
descansomaya <descansomaya@gmail.com>
2026-04-08 12:54:27 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,9 @@
 // ==========================================
 
 App.api = { 
-    gasUrl: "https://script.google.com/macros/s/AKfycbxL3KzjesyZIfiC-Dyr0SwwzwNnPsv5FgHpt-JhyscNpN1eTvRwAh_rdgoxdVnKTAwu/exec",
+    // Aquí ya está tu enlace real restaurado 👇
+    gasUrl: "https://script.google.com/macros/s/AKfycbxL3KzjesyZIfiC-Dyr0SwwzwNnPsv5FgHpt-JhyscNpN1eTvRwAh_rdgoxdVnKTAwu/exec", 
+    
     async fetch(action, payload = {}) { 
         try { 
             const response = await fetch(this.gasUrl, { 
@@ -11,6 +13,8 @@ App.api = {
                 body: JSON.stringify({ action: action, payload: payload, pin: App.state.pinAcceso }) 
             }); 
             const data = await response.json(); 
+            
+            // Si la base de datos rechaza el PIN explícitamente
             if (data.message === "Acceso Denegado. PIN incorrecto.") { 
                 localStorage.removeItem('erp_pin'); 
                 App.state.pinAcceso = null; 
@@ -19,6 +23,7 @@ App.api = {
             return data; 
         } 
         catch (error) { 
+            // Si no hay internet o el enlace está roto
             return { status: "error", message: "Fallo de conexión." }; 
         } 
     } 
