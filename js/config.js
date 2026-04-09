e714e4b18f6aff9679aa8a848ea91328c72cd054
descansomaya <descansomaya@gmail.com>
2026-04-09 13:41:57 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,18 @@
 window.App = window.App || {};
 
 App.config = {
-  version: '11.6.0',
-  appName: 'ERP Descanso Maya',
-  debug: true,
+    version: '11.6.0',
+    appName: 'ERP Descanso Maya',
+    debug: true,
 
-  api: {
-    gasUrl: 'https://script.google.com/macros/s/TU_DEPLOYMENT_ID/exec',
-    timeoutMs: 30000
-  },
+    api: {
+        gasUrl: "https://script.google.com/macros/s/AKfycbxL3KzjesyZIfiC-Dyr0SwwzwNnPsv5FgHpt-JhyscNpN1eTvRwAh_rdgoxdVnKTAwu/exec",
+        timeoutMs: 30000
+    },
 
-  ui: {
-    defaultView: 'inicio',
-    enableCompatStyles: true,
-    useSafeHTML: true
-  }
+    ui: {
+        defaultView: 'inicio',
+        enableCompatStyles: true,
+        useSafeHTML: true
+    }
 };
