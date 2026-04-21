771ecc6efc41a4042cd3f027b94bf7e4c360ce0f
descansomaya <descansomaya@gmail.com>
2026-04-21 15:09:15 -0600

    refactor(finanzas): mover nomina a modulo independiente

---
+++ js/views/views.finanzas.nomina.js
@@ -0,0 +1,11 @@
+window.App = window.App || {};
+App.views = App.views || {};
+
+App.views.nomina = function () {
+    return `
+        <div class="dm-section">
+            <h2>Nómina</h2>
+            <p>Módulo listo para pagos a artesanos.</p>
+        </div>
+    `;
+};
