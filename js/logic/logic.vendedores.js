5688bca733d3fa03e057eaea4c05b8dd762bb41a
descansomaya <descansomaya@gmail.com>
2026-08-13 10:33:42 -0600

    Fix vendedores syntax error

--- js/logic/logic.vendedores.js
+++ js/logic/logic.vendedores.js
@@ -5,8 +5,6 @@
 window.App = window.App || {};
 App.logic = App.logic || {};
 
-Object.assign(App.logic, {
-
 App.logic.obtenerEstadoComisiones = function(vendedorId) {
     // Filtramos los pedidos asignados a este vendedor que tengan alguna comisión
     const pedidosVendedor = (App.state.pedidos || []).filter(p => p.vendedor_id === vendedorId && parseFloat(p.comision || 0) > 0);
@@ -55,5 +53,4 @@ App.logic.pagarComisionesVendedor = async function(vendedorId, pedidosIds, total
         console.error("Error pagando comisiones:", error);
         throw error;
     }
-}
 };
